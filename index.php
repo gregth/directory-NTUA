88ff15c0f47d83d869f56d6413440d438425311e
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2015-02-21 18:43:12 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php header( "Content-Type: text/html; charset=iso-8859-7" ); ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>NTUA Index</title>
+    </head>
+    <body>
+        <h1>NTUA Index :)</h1>
+        <?php require( "list.php" ); ?>
+    </body>
+</html>
