98a005ee6883295ac52e39200e20c481c1f0acf7
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2015-02-24 19:28:49 +0200

    MVC, Security Vulnerabilities Fixed, Improves Script, etc

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <meta charset="utf-8">
-  <head>
-      <title>Parse The Directory</title>
-  </head>
-  <body>
-      <h1>Select school to access it's directory:<h1>
-      <form method="POST" action="directory.php" >
-        <select name="school" >
-          <option value="el" >Ηλεκτρολόγοι</option>
-          <option value="mc" >Μηχανολόγοι </option>
-          <option value="rs" >Τοπογράφοι</option>
-          <option value="ar" >Αρχιτέκτονες</option>
-          <option value="ge" >ΣΕΜΦΕ</option>
-        </select>
-
-        <input type="submit" value="Submit" />
-      </form>
-  </body>
-</html>
+<?php
+  include 'views/home.php';
+?>
